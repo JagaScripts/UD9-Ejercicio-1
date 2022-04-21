51cefa69467dd60777a76652174f80e25e509495
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-04-21 20:12:37 +0200

    Agregada la clase producto y modificado el diagrama con precioFinal public

--- src/modelo/Producto.java
+++ src/modelo/Producto.java
@@ -1,12 +1,14 @@
 package modelo;
 
 /**
- * UD9-Ejercicio-1 - modelo - Lavadora
+ * UD9-Ejercicio-1 - modelo - Producto
  *
  * @author Jose Antonio González Alcántara
  * 
  * Fecha de creación 21/04/2022
  */
-public class Lavadora {
+public interface Producto {
 
+	public void precioFinal() ;
+	
 }
