fd30256431e9eff28db67b8df3547224b899f826
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-04-21 20:03:30 +0200

    Agregado en el diagrama lavadora mas interface producto y la clase lLavadora.java

---
+++ src/modelo/Lavadora.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package modelo;
+
+/**
+ * UD9-Ejercicio-1 - modelo - Lavadora
+ *
+ * @author Jose Antonio González Alcántara
+ * 
+ * Fecha de creación 21/04/2022
+ */
+public class Lavadora {
+
+}
